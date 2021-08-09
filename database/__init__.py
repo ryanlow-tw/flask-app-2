3f238ea947d5be7cbbeba5a0e536c6cabab63667
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-09 16:26:56 +0800

    remove load dotenv

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,6 @@
-from dotenv import load_dotenv
 from database.seed_mongo import seed_database
 import os
 
-load_dotenv('.env')
 mongo_url = f'mongodb://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}'
 db_name = os.getenv('DB_NAME')
 collection_name = os.getenv('DB_COL_NAME')
