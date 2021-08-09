fb86e84ce0fe75aee4a66fa6d52124ddc9cdb820
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-10 00:37:27 +0800

    add spark to application folder init

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,13 @@
+import os
+
 from flask import Flask
 from dotenv import load_dotenv
+from pyspark.sql import SparkSession
 
 load_dotenv('.env')
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+spark = SparkSession.builder.appName("books3000").getOrCreate()
+spark_dataframe = spark.read.parquet(os.getenv("BOOKS_3000_PATH"))
 
 import application.routes
