09e1b09d356da0e03192e432b15855addaf35f47
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-09 18:02:34 +0800

    add packages to requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 setup(
     name='application',
     packages=['application'],
     include_package_data=True,
     install_requires=[
-        'flask',
+        required
     ],
 )
