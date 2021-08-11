ef71c1a2299aa28bea7ea95adc1fa679ad1f60fc
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-11 10:04:09 +0800

    revert app to use local build instead of docker

--- mongo_database/__init__.py
+++ mongo_database/__init__.py
@@ -1,8 +1,8 @@
-from mongo_database.seed_mongo import seed_database
 import os
-
-mongo_url = f'mongodb://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}'
+from mongo_database.seed_mongo import seed_database
+# mongo_url = f'mongodb://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/'
+mongo_url = "mongodb://localhost:27017"
 db_name = os.getenv('DB_NAME')
 collection_name = os.getenv('DB_COL_NAME')
+print(mongo_url)
 collection = seed_database(mongo_url, db_name, collection_name)
-
--- mongo_database/seed_mongo.py
+++ mongo_database/seed_mongo.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import json
 import os
 
+
 def seed_database(mongo_url, db_name, collection_name):
     client = MongoClient(mongo_url)
     database = client[db_name]
